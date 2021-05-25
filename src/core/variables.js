9748d2707d81c076d24df794f01607f90482179c
Artem Alesenko <alesenko19961966@gmail.com>
2021-05-25 16:13:08 +0300

    Added cross price convertation

--- src/core/variables.js
+++ src/core/variables.js
@@ -1,3 +1,8 @@
 export const tickersHandlers = new Map();
 export const API_KEY =
   "fb120dc06519b0e15bc20d309984e3e6a4b2742c58e2c69d3be65d154d7b1193";
+export const socket = new WebSocket(
+  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+);
+export const AGGREGATE_INDEX = "5";
+export const INVALID_INDEX = "500";
--- src/core/websockets.js
+++ src/core/websockets.js
@@ -1,29 +1,41 @@
-import { tickersHandlers, API_KEY } from "./variables";
+import {
+  tickersHandlers,
+  socket,
+  INVALID_INDEX,
+  AGGREGATE_INDEX
+} from "./variables";
 
-const AGGREGATE_INDEX = "5";
-const INVALID_INDEX = "500";
-
-const socket = new WebSocket(
-  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
-);
+let BTC;
 
 socket.addEventListener("message", e => {
   const {
     TYPE: type,
     FROMSYMBOL: currency,
     PRICE: newPrice,
-    PARAMETER: sub
+    PARAMETER: sub,
+    TOSYMBOL: link
   } = JSON.parse(e.data);
+  if (currency === "BTC") BTC = newPrice;
 
   if (type === INVALID_INDEX) {
     const coin = JSON.stringify(sub).split("~")[2];
     const handler = tickersHandlers.get(coin) ?? [];
-    handler.forEach(fn => fn(newPrice, type));
+    if (coin === "BTC") return;
+    sendToWebSockets({
+      action: "SubAdd",
+      subs:
+        coin !== "BTC"
+          ? ["5~CCCAGG~BTC~USD", `5~CCCAGG~${coin}~BTC`]
+          : [`5~CCCAGG~${coin}~BTC`]
+    });
+    if (coin) handler.forEach(fn => fn(newPrice, type));
   }
 
   if (type !== AGGREGATE_INDEX || newPrice === undefined) return;
+  const crossPrice = newPrice * BTC;
   const handler = tickersHandlers.get(currency) ?? [];
-  handler.forEach(fn => fn(newPrice, type));
+  if (link === "BTC") handler.forEach(fn => fn(crossPrice, type));
+  else handler.forEach(fn => fn(newPrice, type));
 });
 
 export function sendToWebSockets(message) {
